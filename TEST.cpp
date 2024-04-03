f2954ce7b9ccd8e75e7054340a6c3c4fe9bb1283
miłosz kaszubowski <milosz.kaszubowski@onet.pl>
2024-04-03 10:31:31 +0200

    I am done

--- TEST.cpp
+++ TEST.cpp
@@ -11,52 +11,69 @@ using namespace std;
 #define L 2.5 // liczba okresów sygnału sinus w przedziale T
 #define M 8.0 // amplituda sygnału sinus
 #define PI 3.14159265 // liczba PI
+
 // pomocniczy typ – kolejne bajty danej ’double’
  typedef union { char c[sizeof(double)]; double d; } Box;
 // zmienne globalne w programie
 double u[(int)(1.0*T/h)+1]; // sygnał wejściowy
-double u1p[(int)(1.0*T/h)+1]; // pierwsza pochodna sygnału wejściowego
-double u2p[(int)(1.0*T/h)+1]; // druga pochodna sygnału wejściowego
-double u3p[(int)(1.0*T/h)+1]; // trzecia pochodna sygnału wejściowego
+
 double y[(int)(1.0*T/h)+1]; // sygnał wyjściowy
 double y1p[(int)(1.0*T/h)+1]; // pierwsza pochodna sygnału wyjściowego
 double y2p[(int)(1.0*T/h)+1]; // druga pochodna sygnału wyjściowego
-double y3p[(int)(1.0*T/h)+1]; // trzecia pochodna sygnału wyjściowego
-double y4p[(int)(1.0*T/h)+1]; // czwarta pochodna sygnału wyjściowego
+
 Box z; // zmienna: pojedyncza wartość sygnału (u lub y)
 // program główny
 int main()
 {
  int i,j,total;
- double a3,a2,a1,a0,b3,b2,b1,b0,w;
+ double a2,a1,a0,b0,w;
+ double A,B;
+ 
  // wczytanie parametrów modelu
- a3=0;//cout << "\n a3 = "; cin >> a3;
- a2=2;//cout << "\n a2 = "; cin >> a2;
+ a2=2;//cout << "\n a2 = "; cin >> a2;  //tu powinno być T podane przez usera
  a1=1;//cout << "\n a1 = "; cin >> a1;
  a0=0;//cout << "\n a0 = "; cin >> a0;
- b3=0;//cout << "\n b3 = "; cin >> b3;
- b2=0;//cout << "\n b2 = "; cin >> b2;
- b1=0;//cout << "\n b1 = "; cin >> b1;
  b0=1;//cout << "\n b0 = "; cin >> b0;
+
+ A=4; //ograniczenia dla wejścia
+ B=A;
+
+ cout << "\n T = "; cin >> a2;
+ cout << "\n A = "; cin >> A;
+ cout << "\n B = "; cin >> B;
+
  cout << "\n\n";
+
  total=sizeof(u)/sizeof(u[0]); // rozmiar wektorów danych
  w=2.0*PI*L/T; // częstotliwość sinusoidy
+
  for(i=0; i<total; i++) // sygnał wejściowy i jego pochodne
  {
- u[i]=M*sin(w*i*h); // sygnał wejściowy: u=M*sin(w*t) , t=i*h
- u1p[i]=M*w*cos(w*i*h); // pochodna 1: u’(t)
- u2p[i]=-M*w*w*sin(w*i*h); // pochodna 2: u’’(t)
- u3p[i]=-M*w*w*w*cos(w*i*h); // pochodna 3: u’’’(t)
+    u[i]=M*sin(w*i*h); // sygnał wejściowy: u=M*sin(w*t) , t=i*h
+
+    if(u[i] < A && u[i] > -A)
+    {
+        u[i]=u[i]*(B/A);
+    }
+    else if(u[i] <= -A)
+    {
+        u[i] = -A;
+    }
+    else if(u[i] >= A)
+    {
+        u[i] = A;
+    }
  }
- y[0]=0; y1p[0]=0; y2p[0]=0; y3p[0]=0; // zerowe warunki początkowe
+
+ y[0]=0; y1p[0]=0; y2p[0]=0; // zerowe warunki początkowe
+
  for(i=0; i<total-1; i++) // główna pętla obliczeń
  {
- y4p[i]=-a3*y3p[i]-a2*y2p[i]-a1*y1p[i]-a0*y[i]+b3*u3p[i]+b2*u2p[i]+b1*u1p[i]+b0*u[i];
- y3p[i+1]=y3p[i]+h*y4p[i];
- y2p[i+1]=y2p[i]+h*y3p[i]+(h*h/2.0)*y4p[i];
- y1p[i+1]=y1p[i]+h*y2p[i]+(h*h/2.0)*y3p[i]+(h*h*h/6.0)*y4p[i];
- y[i+1]=y[i]+h*y1p[i]+(h*h/2.0)*y2p[i]+(h*h*h/6.0)*y3p[i]+(h*h*h*h/24.0)*y4p[i];
+ y2p[i+1]=(-a1*y1p[i]-a0*y[i]+b0*u[i])/a2;
+ y1p[i+1]=y1p[i]+h*y2p[i];
+ y[i+1]=y[i]+h*y1p[i]+(h*h/2.0)*y2p[i];
  }
+
  ofstream fileU("fileU.bin",ios::binary); // zapisanie wyników u(t) do pliku
  if (fileU.fail())
  {
@@ -64,6 +81,7 @@ int main()
  (void)getch();
  return(-1);
  }
+
 for(i=0; i<total; i++)
     for(j=0, z.d=u[i]; j<sizeof(z); j++)
         fileU << z.c[j];
